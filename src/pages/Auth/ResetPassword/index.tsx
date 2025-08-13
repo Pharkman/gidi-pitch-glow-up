38dfad45554e6fb8a1437d4e737b0d1794447d3f
Kodewarlock <dapious99@gmail.com>
2025-08-13 12:53:38 +0100

    clean up on reset password validation

--- src/pages/Auth/ResetPassword/index.tsx
+++ src/pages/Auth/ResetPassword/index.tsx
@@ -31,9 +31,11 @@ const ResetPassword = () => {
             <Formik
               initialValues={{ password: "", confirmPassword: "" }}
               validationSchema={Yup.object({
-                email: Yup.string().email("Invalid email").required("Required"),
                 password: Yup.string()
                   .min(6, "Must be at least 6 characters")
+                  .required("Required"),
+                confirmPassword: Yup.string()
+                  .oneOf([Yup.ref("password"), null], "Passwords must match")
                   .required("Required"),
               })}
               onSubmit={handleReset}
